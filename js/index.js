abcc4938d3b181db3e8a2193d7519c19674f96cb
RainbowComet <61245888+RainbowComet@users.noreply.github.com>
2025-06-04 16:58:04 +0700

    -

---
+++ js/index.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('button[type="submit"]').forEach(function(button) {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        console.log('Submit предотвращён');
+    });
+});
+
+document.querySelectorAll('.js-price-btn').forEach(function(button) {
+    button.addEventListener('click', function() {
+        // Удаляем активный класс у всех кнопок
+        document.querySelectorAll('.js-price-btn').forEach(function(btn) {
+            btn.classList.remove('chart__price-item--active');
+        });
+
+        // Добавляем активный класс только на нажатую кнопку
+        this.classList.add('chart__price-item--active');
+    });
+});
+
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
+    hamburger.classList.toggle('open');
+});
